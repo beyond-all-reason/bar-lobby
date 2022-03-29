045bdde767fe1f916629a2c6fb4ce0dfcd7b0989
Jazcash <jaspercashmore@gmail.com>
2022-03-30 00:06:25 +0100

    ditch pixi, too much overhead

--- src/components/battle/MapPreview.vue
+++ src/components/battle/MapPreview.vue
@@ -2,133 +2,212 @@
     <div id="map-canvas-container" class="map-canvas-container">
         <canvas id="map-canvas" />
         <div class="map-toolbar">
-            <Select v-model="startPosType" :options="startPosOptions" :label-by="(option: any) => option.label" :value-by="(option: any) => option.value" />
+            <Select v-model="battle.hostOptions.startPosType" :options="startPosOptions" :label-by="(option: any) => option.label" :value-by="(option: any) => option.value" />
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import * as PIXI from "pixi.js";
-import { onMounted, onUnmounted, ref, watch } from "vue";
-import { MapData } from "@/model/map-data";
+// import * as PIXI from "pixi.js";
+// import { MapData } from "@/model/map-data";
 import { BattleTypes } from "@/model/battle";
 import Select from "@/components/inputs/Select.vue";
+import { onMounted, watch } from "vue";
 
 const battle = window.api.battle.currentBattle;
 
-const startPosType = ref(battle.hostOptions.startPosType);
 const startPosOptions: Array<{ label: string, value: any }> = [
     { label: "Fixed", value: BattleTypes.StartPosType.Fixed },
     { label: "Random", value: BattleTypes.StartPosType.Random },
     { label: "Boxes", value: BattleTypes.StartPosType.ChooseInGame }
 ];
 
-let app: PIXI.Application;
-let mapSprite: PIXI.Sprite;
-let startBoxesGraphics: PIXI.Graphics;
+type Transform = { x: number, y: number, width: number, height: number };
+
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+let textureMap: HTMLImageElement;
+let mapTransform: Transform;
 
 onMounted(async () => {
-    const canvasContainerEl = document.getElementById("map-canvas-container") as HTMLElement;
-    const canvasEl = document.getElementById("map-canvas") as HTMLCanvasElement;
-
-    app = (window as any).app = new PIXI.Application({
-        view: canvasEl,
-        width: 600,
-        height: 600
-    });
-
-    startBoxesGraphics = new PIXI.Graphics();
-    app.stage.addChild(startBoxesGraphics);
+    canvas = document.getElementById("map-canvas") as HTMLCanvasElement;
+    canvas.width = 600;
+    canvas.height = 600;
+    context = canvas.getContext("2d")!;
 
     loadMap();
 
-    // TODO: only watch necessary properties and update relevant bits
     watch(battle, () => {
         loadMap();
     });
-
-    app.ticker.add(() => {
-        console.log("tick");
-    });
-});
-
-onUnmounted(() => {
-    app.destroy();
 });
 
 async function loadMap() {
-    console.log("loading map");
-    unload();
+    reset();
 
-    const map = window.api.content.maps.getMapByFileName(battle.hostOptions.mapFileName);
-
-    if (!map || !map.textureImagePath) {
-        // TODO show missing map image
+    const mapData = window.api.content.maps.getMapByFileName(battle.hostOptions.mapFileName);
+    if (!mapData || !mapData.textureImagePath) {
+        // TODO: missing map image
         return;
     }
 
-    await loadImages(map);
+    textureMap = await loadImage(mapData.textureImagePath);
 
-    const baseTexture = PIXI.BaseTexture.from("textureMap");
-    const aspectRatio = baseTexture.width / baseTexture.height;
-    baseTexture.width = app.view.width;
-    baseTexture.height = app.view.width / aspectRatio;
-    // TODO: test with long vertical map
-    mapSprite = (window as any).sprite = PIXI.Sprite.from(baseTexture);
-    app.stage.addChildAt(mapSprite, 0);
-    mapSprite.y = (app.view.height * 0.5) - (mapSprite.height * 0.5);
-    mapSprite.addChild(startBoxesGraphics);
+    mapTransform = { x: 0, y: 0, width: canvas.width, height: canvas.height };
 
-    startBoxesGraphics.clear();
-    if (battle.hostOptions.startPosType === BattleTypes.StartPosType.ChooseInGame) {
-        for (const allyTeam of battle.allyTeams) {
-            if (allyTeam.startBox) {
-                drawStartBox(allyTeam.startBox);
-            }
+    const widthToHeightRatio = textureMap.width / textureMap.height;
+    if (widthToHeightRatio > 1) {
+        mapTransform.height = mapTransform.height / widthToHeightRatio;
+    } else {
+        mapTransform.width = mapTransform.width * widthToHeightRatio;
+    }
+
+    mapTransform.x = (canvas.width - mapTransform.width) / 2;
+    mapTransform.y = (canvas.height - mapTransform.height) / 2;
+
+    context.drawImage(textureMap, mapTransform.x, mapTransform.y, mapTransform.width, mapTransform.height);
+
+    drawBoxes();
+}
+
+function drawBoxes() {
+    if (battle.hostOptions.startPosType !== BattleTypes.StartPosType.ChooseInGame) {
+        return;
+    }
+
+    for (const allyTeam of battle.allyTeams) {
+        if (allyTeam.startBox) {
+            context.fillStyle = "rgba(0, 255, 0, 0.5)";
+
+            const boxTransform = {
+                x: mapTransform.x + mapTransform.width * allyTeam.startBox.xPercent,
+                y: mapTransform.y + mapTransform.height * allyTeam.startBox.yPercent,
+                width: mapTransform.width * allyTeam.startBox.widthPercent,
+                height: mapTransform.height * allyTeam.startBox.heightPercent
+            };
+            context.fillRect(boxTransform.x, boxTransform.y, boxTransform.width, boxTransform.height);
         }
     }
 }
 
-function unload() {
-    if (mapSprite) {
-        mapSprite.destroy();
-    }
-
-    app.loader.reset();
-    app.loader.destroy();
-    for (const res in app.loader.resources) {
-        delete app.loader.resources[res];
-    }
-
-    PIXI.utils.clearTextureCache();
+function reset() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawStartBox(startBox: BattleTypes.StartBox) {
-    startBoxesGraphics.beginFill(0x00ff00, 0.3);
-    startBoxesGraphics.drawRect(
-        startBox.xPercent * mapSprite.width,
-        startBox.yPercent * mapSprite.height,
-        startBox.widthPercent * mapSprite.width,
-        startBox.heightPercent * mapSprite.height
-    );
-}
-
-function loadImages(map: MapData) {
-    return new Promise<void>(resolve => {
-        if (map.textureImagePath) {
-            app.loader.add("textureMap", map.textureImagePath);
-        }
-        if (map.heightImagePath) {
-            app.loader.add("heightMap", map.heightImagePath);
-        }
-        if (map.metalImagePath) {
-            app.loader.add("metalMap", map.metalImagePath);
-        }
-        if (map.typeImagePath) {
-            app.loader.add("typeMap", map.typeImagePath);
-        }
-
-        app.loader.load(() => resolve());
+function loadImage(url: string) {
+    return new Promise<HTMLImageElement>(resolve => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.src = url;
     });
 }
+
+// let app: PIXI.Application;
+// let mapSprite: PIXI.Sprite;
+// let startBoxesGraphics: PIXI.Graphics;
+
+// onMounted(async () => {
+//     const canvasContainerEl = document.getElementById("map-canvas-container") as HTMLElement;
+//     const canvasEl = document.getElementById("map-canvas") as HTMLCanvasElement;
+
+//     app = (window as any).app = new PIXI.Application({
+//         view: canvasEl,
+//         width: 600,
+//         height: 600
+//     });
+
+//     startBoxesGraphics = new PIXI.Graphics();
+//     app.stage.addChild(startBoxesGraphics);
+
+//     loadMap();
+
+//     // TODO: only watch necessary properties and update relevant bits
+//     watch(battle, () => {
+//         loadMap();
+//     });
+
+//     app.ticker.add(() => {
+//         console.log("tick");
+//     });
+// });
+
+// onUnmounted(() => {
+//     app.destroy();
+// });
+
+// async function loadMap() {
+//     console.log("loading map");
+//     unload();
+
+//     const map = window.api.content.maps.getMapByFileName(battle.hostOptions.mapFileName);
+
+//     if (!map || !map.textureImagePath) {
+//         // TODO show missing map image
+//         return;
+//     }
+
+//     await loadImages(map);
+
+//     const baseTexture = PIXI.BaseTexture.from("textureMap");
+//     const aspectRatio = baseTexture.width / baseTexture.height;
+//     baseTexture.width = app.view.width;
+//     baseTexture.height = app.view.width / aspectRatio;
+//     // TODO: test with long vertical map
+//     mapSprite = (window as any).sprite = PIXI.Sprite.from(baseTexture);
+//     app.stage.addChildAt(mapSprite, 0);
+//     mapSprite.y = (app.view.height * 0.5) - (mapSprite.height * 0.5);
+//     mapSprite.addChild(startBoxesGraphics);
+
+//     startBoxesGraphics.clear();
+//     if (battle.hostOptions.startPosType === BattleTypes.StartPosType.ChooseInGame) {
+//         for (const allyTeam of battle.allyTeams) {
+//             if (allyTeam.startBox) {
+//                 drawStartBox(allyTeam.startBox);
+//             }
+//         }
+//     }
+// }
+
+// function unload() {
+//     if (mapSprite) {
+//         mapSprite.destroy();
+//     }
+
+//     app.loader.reset();
+//     app.loader.destroy();
+//     for (const res in app.loader.resources) {
+//         delete app.loader.resources[res];
+//     }
+
+//     PIXI.utils.clearTextureCache();
+// }
+
+// function drawStartBox(startBox: BattleTypes.StartBox) {
+//     startBoxesGraphics.beginFill(0x00ff00, 0.3);
+//     startBoxesGraphics.drawRect(
+//         startBox.xPercent * mapSprite.width,
+//         startBox.yPercent * mapSprite.height,
+//         startBox.widthPercent * mapSprite.width,
+//         startBox.heightPercent * mapSprite.height
+//     );
+// }
+
+// function loadImages(map: MapData) {
+//     return new Promise<void>(resolve => {
+//         if (map.textureImagePath) {
+//             app.loader.add("textureMap", map.textureImagePath);
+//         }
+//         if (map.heightImagePath) {
+//             app.loader.add("heightMap", map.heightImagePath);
+//         }
+//         if (map.metalImagePath) {
+//             app.loader.add("metalMap", map.metalImagePath);
+//         }
+//         if (map.typeImagePath) {
+//             app.loader.add("typeMap", map.typeImagePath);
+//         }
+
+//         app.loader.load(() => resolve());
+//     });
+// }
 </script>
