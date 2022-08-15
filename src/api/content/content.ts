2a49b6fa2178a112e7b0837c0df1d697b1825bcd
Jazcash <jaspercashmore@gmail.com>
2022-08-16 00:16:12 +0100

    tell server when we've downloaded content

--- src/api/content/content.ts
+++ src/api/content/content.ts
@@ -30,6 +30,57 @@ export class ContentAPI {
         this.currentDownloadCurrent = computed(() => this.downloads.value.reduce((acc, cur) => acc + cur.currentBytes, 0));
         this.currentDownloadTotal = computed(() => this.downloads.value.reduce((acc, cur) => acc + cur.totalBytes, 0));
         this.currentDownloadPercent = computed(() => this.currentDownloadCurrent.value / this.currentDownloadTotal.value);
+
+        this.engine.onDownloadComplete.add((data) => {
+            const battle = api.session.onlineBattle.value;
+            const me = api.session.currentUser;
+
+            if (battle && battle.battleOptions.engineVersion === data.name) {
+                api.comms.request("c.lobby.update_status", {
+                    client: {
+                        sync: {
+                            engine: 1,
+                            game: me.battleStatus.sync.game,
+                            map: me.battleStatus.sync.map,
+                        },
+                    },
+                });
+            }
+        });
+
+        this.game.onDownloadComplete.add((data) => {
+            const battle = api.session.onlineBattle.value;
+            const me = api.session.currentUser;
+
+            if (battle && battle.battleOptions.gameVersion === data.name) {
+                api.comms.request("c.lobby.update_status", {
+                    client: {
+                        sync: {
+                            engine: me.battleStatus.sync.engine,
+                            game: 1,
+                            map: me.battleStatus.sync.map,
+                        },
+                    },
+                });
+            }
+        });
+
+        this.maps.onDownloadComplete.add((data) => {
+            const battle = api.session.onlineBattle.value;
+            const me = api.session.currentUser;
+
+            if (battle && battle.battleOptions.map === data.name) {
+                api.comms.request("c.lobby.update_status", {
+                    client: {
+                        sync: {
+                            engine: me.battleStatus.sync.engine,
+                            game: me.battleStatus.sync.game,
+                            map: 1,
+                        },
+                    },
+                });
+            }
+        });
     }
 
     public async init() {
