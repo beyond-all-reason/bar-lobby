7b90431555a1fb2ef9ba98165f971b8425b13e72
Jazcash <jaspercashmore@gmail.com>
2022-09-26 11:15:37 +0100

    store installed maps

--- src/api/content/replay-content.ts
+++ src/api/content/replay-content.ts
@@ -1,23 +1,16 @@
 import fs from "fs";
+import { delay } from "jaz-ts-utils";
 import path from "path";
 
 import { AbstractContentAPI } from "@/api/content/abstract-content-api";
 import { parseReplay as parseReplayWorkerFunction } from "@/workers/parse-replay";
 import { hookWorkerFunction } from "@/workers/worker-helpers";
 
-/**
- * 1. on launch, scan demos folder and read in all filenames
- * 2. read all filenames of cached demos from db
- * 3. iterate through filenames in #1 and cache any not in #2
- * 4. iterate through filenames in #2 and delete any not in #1
- *
- * - store table of errored replays, so we know to skip over these files in the future to avoid getting stuck in a failing caching loop
- * - after launch, watch the demos dir and run #3 if new files added. run #4 if files deleted
- */
 export class ReplayContentAPI extends AbstractContentAPI {
     public readonly replaysDir = path.join(api.info.contentPath, "demos");
 
     protected parseReplay = hookWorkerFunction(new Worker(new URL(`../../workers/parse-replay.ts`, import.meta.url), { type: "module" }), parseReplayWorkerFunction);
+    protected replayCacheQueue: Set<string> = new Set();
 
     public override async init() {
         await fs.promises.mkdir(this.replaysDir, { recursive: true });
@@ -26,8 +19,8 @@ export class ReplayContentAPI extends AbstractContentAPI {
             .createTable("replay")
             .ifNotExists()
             .addColumn("replayId", "integer", (col) => col.primaryKey().autoIncrement())
-            .addColumn("gameId", "varchar", (col) => col.notNull())
-            .addColumn("fileName", "varchar", (col) => col.notNull())
+            .addColumn("gameId", "varchar", (col) => col.notNull().unique())
+            .addColumn("fileName", "varchar", (col) => col.notNull().unique())
             .addColumn("engineVersion", "varchar", (col) => col.notNull())
             .addColumn("gameVersion", "varchar", (col) => col.notNull())
             .addColumn("mapScriptName", "varchar", (col) => col.notNull())
@@ -55,13 +48,15 @@ export class ReplayContentAPI extends AbstractContentAPI {
             .addColumn("fileName", "varchar", (col) => col.primaryKey())
             .execute();
 
-        this.cacheReplays().then(() => {
-            fs.watch(this.replaysDir, (watchEvent, filename) => {
-                if (watchEvent === "change") {
-                    console.log(filename);
-                }
-            });
+        await this.queueReplaysToCache();
+
+        fs.watch(this.replaysDir, (watchEvent, filename) => {
+            if (watchEvent === "change") {
+                this.replayCacheQueue.add(filename);
+            }
         });
+
+        this.cacheReplays();
 
         return super.init();
     }
@@ -79,7 +74,7 @@ export class ReplayContentAPI extends AbstractContentAPI {
         return num_replays;
     }
 
-    protected async cacheReplays() {
+    protected async queueReplaysToCache() {
         const replayFiles = await fs.promises.readdir(this.replaysDir);
 
         const cachedReplayFiles = await api.cacheDb.selectFrom("replay").select(["fileName"]).execute();
@@ -88,36 +83,48 @@ export class ReplayContentAPI extends AbstractContentAPI {
         const erroredReplayFiles = await api.cacheDb.selectFrom("replayError").select(["fileName"]).execute();
         const erroredReplayFileNames = erroredReplayFiles.map((file) => file.fileName);
 
-        for (const replayFileName of replayFiles) {
-            if (cachedReplayFileNames.includes(replayFileName) || erroredReplayFileNames.includes(replayFileName)) {
-                continue;
-            }
+        const replaysFilesToCache = replayFiles.filter((file) => !cachedReplayFileNames.includes(file) && !erroredReplayFileNames.includes(file));
 
-            try {
-                await this.cacheReplay(replayFileName);
-            } catch (err) {
-                console.error(`Error parsing replay: ${replayFileName}`, err);
-
-                await api.cacheDb
-                    .insertInto("replayError")
-                    .values({
-                        fileName: replayFileName,
-                    })
-                    .execute();
-            }
+        for (const replayFileToCache of replaysFilesToCache) {
+            this.replayCacheQueue.add(replayFileToCache);
         }
     }
 
+    protected async cacheReplays() {
+        const [replayToCache] = this.replayCacheQueue;
+
+        if (replayToCache) {
+            await this.cacheReplay(replayToCache);
+        } else {
+            await delay(5000);
+        }
+
+        this.cacheReplays();
+    }
+
     protected async cacheReplay(replayFileName: string) {
-        console.debug(`Caching: ${replayFileName}`);
-        console.time(`Cached: ${replayFileName}`);
+        try {
+            console.debug(`Caching: ${replayFileName}`);
+            console.time(`Cached: ${replayFileName}`);
 
-        const replayFilePath = path.join(api.info.contentPath, "demos", replayFileName);
+            const replayFilePath = path.join(api.info.contentPath, "demos", replayFileName);
 
-        const replayData = await this.parseReplay(replayFilePath);
+            const replayData = await this.parseReplay(replayFilePath);
 
-        await api.cacheDb.insertInto("replay").values(replayData).execute();
+            await api.cacheDb.insertInto("replay").values(replayData).execute();
 
-        console.timeEnd(`Cached: ${replayFileName}`);
+            console.timeEnd(`Cached: ${replayFileName}`);
+        } catch (err) {
+            console.error(`Error parsing replay: ${replayFileName}`, err);
+
+            await api.cacheDb
+                .insertInto("replayError")
+                .values({
+                    fileName: replayFileName,
+                })
+                .execute();
+        }
+
+        this.replayCacheQueue.delete(replayFileName);
     }
 }
