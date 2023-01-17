674e82d029b8b6c5d3dfecf35727c4700bbf9da0
Jazcash <jaspercashmore@gmail.com>
2023-01-17 00:54:31 +0000

    ignore typed-router.d.ts for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
     extends: ["./node_modules/jaz-ts-utils/.eslintrc"],
     env: {
         node: true,
-        browser: true
+        browser: true,
     },
+    ignorePatterns: ["typed-router.d.ts"],
     rules: {
         // Rules should only be added here for testing temporarily and should eventually be moved into jaz-ts-utils to ensure consistency across projects
     },
