6b5bc3edb3ac54064a1450bb50d58e65cfc3c54c
Jazcash <jaspercashmore@gmail.com>
2023-03-05 16:40:47 +0000

    style tweaks

--- src/renderer/components/misc/ScenarioTile.vue
+++ src/renderer/components/misc/ScenarioTile.vue
@@ -25,7 +25,6 @@ const backgroundImageCss = computed(() => `url('${props.scenario.imagepath}')`);
     align-items: flex-end;
     justify-content: flex-start;
     position: relative;
-    //border-top: 1px solid rgba(255, 255, 255, 0.3);
     border: 4px solid rgba(0, 0, 0, 0.2);
     outline: 1px solid rgba(255, 255, 255, 0.1);
     outline-offset: -1px;
@@ -40,21 +39,22 @@ const backgroundImageCss = computed(() => `url('${props.scenario.imagepath}')`);
         background-repeat: no-repeat;
         transform: scale(1.1);
         transition: 0.1s all;
-        filter: brightness(1) contrast(1);
+        filter: saturate(0) brightness(0.8) contrast(1);
         will-change: transform, filter;
     }
     &:after {
         @extend .fullsize;
         z-index: 1;
-        background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7));
+        background: linear-gradient(rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0.9));
         outline: 1px solid rgba(255, 255, 255, 0.15);
         outline-offset: -1px;
+        transition: 0.1s all;
     }
     &:hover,
     &.selected {
         &:before {
             transform: scale(1);
-            filter: brightness(1.5) contrast(1.1);
+            filter: saturate(1) brightness(1.1) contrast(1.1);
         }
         outline: 1px solid rgba(255, 255, 255, 0.5);
     }
@@ -65,6 +65,8 @@ const backgroundImageCss = computed(() => `url('${props.scenario.imagepath}')`);
     font-weight: 500;
     z-index: 2;
     padding: 10px;
+    padding-bottom: 6px;
     width: 100%;
+    transition: 0.1s all;
 }
 </style>
