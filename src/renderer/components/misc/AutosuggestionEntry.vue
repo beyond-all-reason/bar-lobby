521e8df51eeb545706328ba8335f700bff7d2cc1
JasonReed-2 <Jreed21649@gmail.com>
2023-06-14 00:03:19 -0500

    experimenting

---
+++ src/renderer/components/misc/AutosuggestionEntry.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>{{ props.command }}</h3>
+        <p>{{ props.description }}</p>
+        <input type="text" list="test" />
+        <datalist id="test">
+            <option>hello</option>
+        </datalist>
+    </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+    command: string;
+    description: string;
+}>();
+</script>
+<style lang="scss" scoped>
+</style>
