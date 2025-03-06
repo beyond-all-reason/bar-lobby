44322bedf762e3a06fd6ee6e8e8433bbf4676ee0
marGoira <mnopri82@gmail.com>
2025-03-06 23:01:10 +0200

    fixed format typo

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,7 +19,7 @@ import { autoUpdaterService } from "@main/services/auto-updater.service";
 import { replayContentAPI } from "@main/content/replays/replay-content";
 import { authService } from "@main/services/auth.service";
 import { tachyonService } from "@main/services/tachyon.service";
-import netFromNode from 'node:net'; 
+import netFromNode from "node:net";
 
 // Enable happy eyeballs for IPv6/IPv4 dual stack.
 netFromNode.setDefaultAutoSelectFamily(true);
