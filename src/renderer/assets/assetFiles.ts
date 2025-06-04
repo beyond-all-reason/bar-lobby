474e662de3ac32d0cb49f7e167d0b7802aea9cd2
Marek Rusinowski <marekrusinowski@gmail.com>
2025-06-05 01:22:56 +0200

    Filter out .license files when loading assets

--- src/renderer/assets/assetFiles.ts
+++ src/renderer/assets/assetFiles.ts
@@ -3,17 +3,17 @@
 // The value could be an url or a data: url. Check out sfxFiles in production app for example
 
 // Sounds
-export const musicFiles = import.meta.glob<string>("./audio/music/*", { eager: true, import: "default", query: "?url" });
-export const sfxFiles = import.meta.glob<string>("./audio/sfx/*", { eager: true, import: "default", query: "?url" });
+export const musicFiles = import.meta.glob<string>(["./audio/music/*", "!**/*.license"], { eager: true, import: "default", query: "?url" });
+export const sfxFiles = import.meta.glob<string>(["./audio/sfx/*", "!**/*.license"], { eager: true, import: "default", query: "?url" });
 
 // Videos
-export const introVideos = import.meta.glob<string>("./videos/intros/*", { eager: true, import: "default", query: "?url" });
+export const introVideos = import.meta.glob<string>(["./videos/intros/*", "!**/*.license"], { eager: true, import: "default", query: "?url" });
 
 // Images
-export const backgroundImages = import.meta.glob<string>("./images/backgrounds/*", { eager: true, import: "default", query: "?url" });
+export const backgroundImages = import.meta.glob<string>(["./images/backgrounds/*", "!**/*.license"], { eager: true, import: "default", query: "?url" });
 
 // Fonts
-export const fontFiles = import.meta.glob<string>("./fonts/*", { eager: true, import: "default", query: "?url" });
+export const fontFiles = import.meta.glob<string>(["./fonts/*", "!**/*.license"], { eager: true, import: "default", query: "?url" });
 
 // Languages
-export const localeFilePaths = import.meta.glob<Record<string, string>>("./languages/*", { eager: true, import: "default", query: "?url" });
+export const localeFilePaths = import.meta.glob<Record<string, string>>(["./languages/*", "!**/*.license"], { eager: true, import: "default", query: "?url" });
