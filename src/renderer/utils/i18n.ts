a41977a1309734cf3aab4b0caeceb18e32d65e73
Marek Rusinowski <marekrusinowski@gmail.com>
2025-06-14 17:04:14 +0200

    Add license for new file

--- src/renderer/utils/i18n.ts
+++ src/renderer/utils/i18n.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 The BAR Lobby Authors
+//
+// SPDX-License-Identifier: MIT
+
 export const pluralize = (str: string, quantity: number, locale: string = "en-US"): string => {
     const rule = new Intl.PluralRules(locale).select(quantity);
     if (rule == "one") return str;
