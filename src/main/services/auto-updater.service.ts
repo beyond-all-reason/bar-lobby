5e6f0c2b48803b68de027244ac6549654a62a187
webdevsavvy <devmail.adrianabreu@gmail.com>
2025-06-15 12:56:01 +0100

    remove unnecessary export

--- src/main/services/auto-updater.service.ts
+++ src/main/services/auto-updater.service.ts
@@ -9,43 +9,41 @@ const { autoUpdater } = electronUpdater;
 
 const log = logger("auto-updater.service.ts");
 
+// Examples: https://github.com/iffy/electron-updater-example/blob/master/main.js
 async function init() {
     if (app.isPackaged || process.env.NODE_ENV === "production") {
         log.info("Initializing Auto-Updater Service");
-        registerEvents();
+
         autoUpdater.logger = log;
+
+        autoUpdater.on("checking-for-update", () => {
+            log.info("Checking for updates...");
+        });
+
+        autoUpdater.on("update-available", (info) => {
+            log.info("Update available");
+        });
+
+        autoUpdater.on("update-not-available", (info) => {
+            log.info("Update not available");
+        });
+
+        autoUpdater.on("error", (err) => {
+            log.error("Error trying to update");
+        });
+
+        autoUpdater.on("download-progress", (progressObj) => {
+            log.info("Download progress");
+        });
+
+        autoUpdater.on("update-downloaded", (info) => {
+            log.info("Update downloaded");
+        });
+
         await autoUpdater.checkForUpdatesAndNotify(); // this will automatically install updates once the app quits
     }
 }
 
-// Examples: https://github.com/iffy/electron-updater-example/blob/master/main.js
-function registerEvents() {
-    autoUpdater.on("checking-for-update", () => {
-        log.info("Checking for updates...");
-    });
-
-    autoUpdater.on("update-available", (info) => {
-        log.info("Update available");
-    });
-
-    autoUpdater.on("update-not-available", (info) => {
-        log.info("Update not available");
-    });
-
-    autoUpdater.on("error", (err) => {
-        log.error("Error trying to update");
-    });
-
-    autoUpdater.on("download-progress", (progressObj) => {
-        log.info("Download progress");
-    });
-
-    autoUpdater.on("update-downloaded", (info) => {
-        log.info("Update downloaded");
-    });
-}
-
 export const autoUpdaterService = {
     init,
-    registerEvents,
 };
