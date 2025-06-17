ed6ae58a3b8b56ac868e562f1665755311a0139d
webdevsavvy <devmail.adrianabreu@gmail.com>
2025-06-17 02:11:35 +0100

    move logic for auto updates to an encapsulated class

---
+++ src/main/content/auto-updater.ts
@@ -0,0 +1,107 @@
+// SPDX-FileCopyrightText: 2025 The BAR Lobby Authors
+//
+// SPDX-License-Identifier: MIT
+
+import { AbstractContentAPI } from "@main/content/abstract-content";
+import { DownloadInfo } from "@main/content/downloads";
+import { logger } from "@main/utils/logger";
+import { app } from "electron";
+import electronUpdater from "electron-updater";
+const { autoUpdater } = electronUpdater;
+
+const log = logger("auto-updater.ts");
+
+export type ReleaseNoteInfo = {
+    readonly version: string;
+    readonly note: string | null;
+};
+
+export type UpdateInfo = {
+    version: string;
+    releaseName: string | null;
+    releaseNotes: string | Array<ReleaseNoteInfo> | null;
+    releaseDate: string;
+};
+
+export class AutoUpdaterAPI extends AbstractContentAPI<string, UpdateInfo> {
+    public isVersionInstalled(id: string): boolean {
+        return id === this.updateInfo?.version;
+    }
+
+    public uninstallVersion(version: string | UpdateInfo): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    private updateInfo?: UpdateInfo;
+    private intialized: boolean = false;
+
+    public override async init() {
+        log.info("Initializing Auto-Updater");
+        if (!app.isPackaged) {
+            log.info("App is not packaged. Skipping initializaion");
+            return this;
+        }
+        autoUpdater.autoDownload = false;
+        autoUpdater.logger = log;
+        autoUpdater.autoInstallOnAppQuit = true;
+        this.intialized = true;
+        return this;
+    }
+
+    public async checkForUpdates(): Promise<boolean> {
+        if (!this.intialized) return false;
+        return await new Promise<boolean>((resolve) => {
+            autoUpdater.on("update-available", (info) => {
+                this.updateInfo = {
+                    version: info.version,
+                    releaseName: info.releaseName,
+                    releaseNotes: info.releaseNotes,
+                    releaseDate: info.releaseDate,
+                } as UpdateInfo;
+                resolve(true);
+            });
+
+            autoUpdater.on("update-not-available", () => {
+                resolve(false);
+            });
+
+            autoUpdater.on("error", () => {
+                log.error("Error downloading update");
+                resolve(false);
+            });
+
+            autoUpdater.checkForUpdates();
+        });
+    }
+
+    public async downloadUpdate(): Promise<void> {
+        if (!this.intialized) return;
+        return await new Promise<void>((resolve) => {
+            autoUpdater.on("download-progress", (progressInfo) => {
+                this.downloadProgress({
+                    type: "update",
+                    name: this.updateInfo?.version,
+                    currentBytes: progressInfo.transferred,
+                    totalBytes: progressInfo.total,
+                } as DownloadInfo);
+            });
+
+            autoUpdater.on("update-downloaded", () => {
+                resolve();
+            });
+
+            autoUpdater.on("error", (error) => {
+                log.error(error, "Error downloading update");
+                resolve();
+            });
+
+            autoUpdater.downloadUpdate();
+        });
+    }
+
+    public quitAndInstall(): void {
+        autoUpdater.quitAndInstall();
+    }
+}
+
+export const autoUpdaterAPI = new AutoUpdaterAPI();
--- src/main/services/auto-updater.service.ts
+++ src/main/services/auto-updater.service.ts
@@ -2,87 +2,25 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { BarIpcWebContents, ipcMain } from "@main/typed-ipc";
-import { logger } from "@main/utils/logger";
-import { app } from "electron";
-import electronUpdater from "electron-updater";
-const { autoUpdater } = electronUpdater;
-
-const log = logger("auto-updater.service.ts");
+import { ipcMain } from "@main/typed-ipc";
+import { autoUpdaterAPI } from "@main/content/auto-updater";
 
 // Examples: https://github.com/iffy/electron-updater-example/blob/master/main.js
 async function init() {
-    if (app.isPackaged || process.env.NODE_ENV === "production") {
-        log.info("Initializing Auto-Updater Service");
-
-        autoUpdater.logger = log;
-
-        autoUpdater.autoInstallOnAppQuit = true;
-
-        // autoUpdater.on("checking-for-update", () => {
-        //     log.info("Checking for updates...");
-        // });
-
-        // autoUpdater.on("update-available", (info) => {
-        //     log.info(`Update available: ${info.version}`);
-        // });
-
-        // autoUpdater.on("update-not-available", (info) => {
-        //     log.info(`Update not available: ${info.version}`);
-        // });
-
-        // autoUpdater.on("error", (err) => {
-        //     log.error(`Error trying to update: ${err.message}`);
-        // });
-
-        // autoUpdater.on("download-progress", (progressObj) => {
-        //     log.info(`Download progress: ${progressObj.percent}/100`);
-        // });
-
-        // autoUpdater.on("update-downloaded", (info) => {
-        //     log.info(`Update downloaded: ${info.version}`);
-        // });
-
-        // await autoUpdater.checkForUpdatesAndNotify(); // this will automatically install updates once the app quits
-    }
+    autoUpdaterAPI.init();
 }
 
-function registerIpcHandlers(webContents: BarIpcWebContents) {
-    autoUpdater.on("checking-for-update", () => {
-        webContents.send("autoUpdater:checkingForUpdate");
-    });
-
-    autoUpdater.on("update-available", (info) => {
-        webContents.send("autoUpdater:updateAvailable");
-    });
-
-    autoUpdater.on("update-not-available", (info) => {
-        webContents.send("autoUpdater:updateNotAvailable");
-    });
-
-    autoUpdater.on("error", (err) => {
-        webContents.send("autoUpdater:error");
-    });
-
-    autoUpdater.on("download-progress", (progressObj) => {
-        webContents.send("downloads:update:progress", {
-            type: "update",
-            name: "client-update",
-            currentBytes: progressObj.transferred,
-            totalBytes: progressObj.total,
-        });
-    });
-
-    autoUpdater.on("update-downloaded", (info) => {
-        webContents.send("downloads:update:complete");
-    });
-
+function registerIpcHandlers() {
     ipcMain.handle("autoUpdater:checkForUpdates", async () => {
-        await autoUpdater.checkForUpdates();
+        return await autoUpdaterAPI.checkForUpdates();
+    });
+
+    ipcMain.handle("autoUpdater:downloadUpdate", async () => {
+        return await autoUpdaterAPI.downloadUpdate();
     });
 
     ipcMain.handle("autoUpdater:quitAndInstall", () => {
-        autoUpdater.quitAndInstall();
+        autoUpdaterAPI.quitAndInstall();
     });
 }
 
