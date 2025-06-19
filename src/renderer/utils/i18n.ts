85e813b2b1aca71e26ff0c3bd9d2ab1ac8fcf3e7
Adrian <85309342+webdevsavvy@users.noreply.github.com>
2025-06-19 21:16:58 +0100

    Add basic support for internatiolization (#418)
    This PR aims to add basic support for i18n through game translation
    files and vue-i18n
    
    Contributes to #316 
    
    ### Added
    - Copied translation files from main game repo into assets folder
    - Utility function for processing translation file data
    - Bash script to look for translation targets
    
    ### Changed
    - Modified the setup of `vue-i18n` to manage new translation file
    directory structure

--- src/renderer/utils/i18n.ts
+++ src/renderer/utils/i18n.ts
@@ -7,3 +7,12 @@ export const pluralize = (str: string, quantity: number, locale: string = "en-US
     if (rule == "one") return str;
     return str + "s"; // TODO: link to i18n translation files
 };
+
+export const processTranslationData = (jsonData) => {
+    for (const key in jsonData) {
+        if (typeof jsonData[key] === "object") {
+            jsonData[key] = processTranslationData(jsonData[key]);
+        } else if (typeof jsonData[key] === "string") jsonData[key] = jsonData[key].replaceAll(/%\{(.*?)\}/g, "{$1}");
+    }
+    return jsonData;
+};
