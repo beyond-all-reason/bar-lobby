9bfeadca900135248fe34cc1e6af423f349951ef
webdevsavvy <devmail.adrianabreu@gmail.com>
2025-06-22 09:32:20 +0100

    refactor for dry code

--- src/renderer/i18n.ts
+++ src/renderer/i18n.ts
@@ -13,34 +13,29 @@ import zhTranslation from "@renderer/assets/languages/zh.json";
 type Locale = "cs" | "de" | "en" | "fr" | "ru" | "zh";
 type MessageSchema = typeof enTranslation | typeof csTranslation | typeof deTranslation | typeof frTranslation | typeof ruTranslation | typeof zhTranslation;
 
+const messages = {
+    cs: csTranslation,
+    de: deTranslation,
+    en: enTranslation,
+    fr: frTranslation,
+    ru: ruTranslation,
+    zh: zhTranslation,
+};
+
 export async function setupI18n() {
     const myLocale = Intl.DateTimeFormat().resolvedOptions().locale.split("-")[0]; // TODO: add override from user settings
 
     return createI18n<[MessageSchema], Locale>({
         locale: myLocale,
         fallbackLocale: "en",
-        messages: {
-            cs: csTranslation,
-            de: deTranslation,
-            en: enTranslation,
-            fr: frTranslation,
-            ru: ruTranslation,
-            zh: zhTranslation,
-        },
+        messages,
         legacy: false,
     });
 }
 
 export function useTypedI18n(): ReturnType<typeof useI18n<{ message: MessageSchema }, Locale>> {
     return useI18n<{ message: MessageSchema }, Locale>({
-        messages: {
-            cs: csTranslation,
-            de: deTranslation,
-            en: enTranslation,
-            fr: frTranslation,
-            ru: ruTranslation,
-            zh: zhTranslation,
-        },
+        messages,
         useScope: "global",
     });
 }
