508583a56fe4a15e7a913f89750a5b0d4e2c707e
webdevsavvy <devmail.adrianabreu@gmail.com>
2025-06-22 08:37:11 +0100

    rework i18n implementation for adding typescript support

---
+++ src/renderer/i18n.ts
@@ -0,0 +1,42 @@
+import { createI18n, useI18n } from "vue-i18n";
+import enTranslation from "@renderer/assets/languages/en.json";
+import csTranslation from "@renderer/assets/languages/cs.json";
+import deTranslation from "@renderer/assets/languages/de.json";
+import frTranslation from "@renderer/assets/languages/fr.json";
+import ruTranslation from "@renderer/assets/languages/ru.json";
+import zhTranslation from "@renderer/assets/languages/zh.json";
+
+type Locale = "cs" | "de" | "en" | "fr" | "ru" | "zh";
+type MessageSchema = typeof enTranslation | typeof csTranslation | typeof deTranslation | typeof frTranslation | typeof ruTranslation | typeof zhTranslation;
+
+export async function setupI18n() {
+    const myLocale = Intl.DateTimeFormat().resolvedOptions().locale.split("-")[0]; // TODO: add override from user settings
+ 
+    return createI18n<[MessageSchema], Locale>({
+        locale: myLocale,
+        fallbackLocale: "en",
+        messages: {
+            cs: csTranslation,
+            de: deTranslation,
+            en: enTranslation,
+            fr: frTranslation,
+            ru: ruTranslation,
+            zh: zhTranslation
+        },
+        legacy: false,
+    });
+}
+
+export function useTypedI18n(): ReturnType<typeof useI18n<{ message: MessageSchema }, Locale>> {
+    return useI18n<{ message: MessageSchema }, Locale>({
+        messages: {
+            cs: csTranslation,
+            de: deTranslation,
+            en: enTranslation,
+            fr: frTranslation,
+            ru: ruTranslation,
+            zh: zhTranslation,
+        },
+        useScope: "global",
+    });
+}
