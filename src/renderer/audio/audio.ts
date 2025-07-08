81859e0160cb7370369285a4742779eb11c3d656
Benoit Coudour <benoit.coudour@gmail.com>
2025-07-08 21:41:22 +0200

    Fix music volume issues.

--- src/renderer/audio/audio.ts
+++ src/renderer/audio/audio.ts
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { Settings } from "@main/services/settings.service";
 import { musicFiles, sfxFiles } from "@renderer/assets/assetFiles";
 import { GameStatus, gameStore } from "@renderer/store/game.store";
 import { settingsStore } from "@renderer/store/settings.store";
@@ -24,19 +23,17 @@ class Sound extends Howl {
 class AudioAPI {
     public sounds: Map<string, Sound> = new Map();
 
-    private settings: Settings | undefined;
-
     public async init() {
         if (this.sounds.size) {
             return this;
         }
-        this.settings = await window.settings.getSettings();
+
         console.debug("Loading music files...");
         for (const filePath in musicFiles) {
             const name = filePath.split("/").pop()?.split(".")[0];
             console.debug(name);
             const src = musicFiles[filePath];
-            const volume = this.settings.musicVolume / 100;
+            const volume = settingsStore.musicVolume / 100;
 
             if (!name) {
                 console.error(`something wrong with audio file located at ${filePath}`);
@@ -59,7 +56,7 @@ class AudioAPI {
             const name = filePath.split("/").pop()?.split(".")[0] || "";
             console.debug(name);
             const src = sfxFiles[filePath];
-            const volume = this.settings.sfxVolume / 100;
+            const volume = settingsStore.sfxVolume / 100;
             const sound = new Sound(name, false, { src, volume, preload: false, html5: true });
             this.sounds.set(name, sound);
         }
@@ -129,10 +126,11 @@ class AudioAPI {
     }
 
     public unmuteMusic(fadeTime = 4000) {
-        const musicSounds = this.getAllSounds().filter((sound) => sound.isMusic);
-        if (!this.settings) throw new Error("failed to access settings");
-        for (const sound of musicSounds) {
-            sound.fade(0, (this.settings.musicVolume || 0) / 100, fadeTime);
+        if (settingsStore.musicVolume > 0) {
+            const musicSounds = this.getAllSounds().filter((sound) => sound.isMusic);
+            for (const sound of musicSounds) {
+                sound.fade(0, settingsStore.musicVolume / 100, fadeTime);
+            }
         }
     }
 }
