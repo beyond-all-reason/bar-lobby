ff75e1f3e0732d4428f523123c52f9388cd7700e
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2025-07-31 20:41:19 -0400

    i18n MapFiltersComponent

--- src/renderer/components/maps/MapFiltersComponent.vue
+++ src/renderer/components/maps/MapFiltersComponent.vue
@@ -8,20 +8,20 @@ SPDX-License-Identifier: MIT
     <div class="fullheight scroll-container">
         <div class="flex-col gap-xl filters">
             <div>
-                <h6>Max Players</h6>
+                <h6>{{ t("lobby.components.maps.mapFiltersComponent.maxPlayers") }}</h6>
                 <MaxPlayersFilter />
             </div>
             <div>
-                <h6>Filter By</h6>
+                <h6>{{ t("lobby.components.maps.mapFiltersComponent.filterBy") }}</h6>
                 <FavoritesFilter />
                 <DownloadedFilter />
             </div>
             <div>
-                <h6>Terrain</h6>
+                <h6>{{ t("lobby.components.maps.mapFiltersComponent.terrain") }}</h6>
                 <TerrainFilter />
             </div>
             <div>
-                <h6>Game Type</h6>
+                <h6>{{ t("lobby.components.maps.mapFiltersComponent.gameType") }}</h6>
                 <GameTypeFilter />
             </div>
         </div>
@@ -49,6 +49,8 @@ import MaxPlayersFilter from "@renderer/components/maps/filters/MaxPlayersFilter
 import TerrainFilter from "@renderer/components/maps/filters/TerrainFilter.vue";
 import FavoritesFilter from "@renderer/components/maps/filters/FavoritesFilter.vue";
 import DownloadedFilter from "@renderer/components/maps/filters/DownloadedFilter.vue";
+import { useTypedI18n } from "@renderer/i18n";
+const { t } = useTypedI18n();
 </script>
 
 <style lang="scss" scoped>
