64f5338b625abb6948d634b171942e45d93a68e8
James Gardiner <wrath625@gmail.com>
2025-08-01 23:28:52 -0700

    Replace hard coded strings with translations for battle files (#425) (I18N) (#462)
    Hardcoded string replacement for @webdevsavvy's issue #425 specifically
    for the battle views

--- src/renderer/components/battle/HostBattle.vue
+++ src/renderer/components/battle/HostBattle.vue
@@ -5,14 +5,21 @@ SPDX-License-Identifier: MIT
 -->
 
 <template>
-    <Modal title="Host Battle" width="400px" @open="onOpen" @close="onClose">
+    <Modal :title="t('lobby.components.battle.hostBattle.title')" width="400px" @open="onOpen" @close="onClose">
         <div class="flex-col gap-md">
             <template v-if="waitingForBattleCreation">
-                <div class="txt-center">Setting up a dedicated battle host, this usually takes around 30 seconds</div>
+                <div class="txt-center">{{ t("lobby.components.battle.hostBattle.settingUp") }}</div>
                 <Loader :absolutePosition="false"></Loader>
             </template>
             <template v-else>
-                <Select v-model="selectedRegion" :options="regions" label="Region" optionLabel="name" optionValue="code" class="fullwidth">
+                <Select
+                    v-model="selectedRegion"
+                    :options="regions"
+                    :label="t('lobby.components.battle.hostBattle.region')"
+                    optionLabel="name"
+                    optionValue="code"
+                    class="fullwidth"
+                >
                     <template #value>
                         <div class="flex-row gap-md">
                             <Flag :countryCode="selectedRegion" />
@@ -27,7 +34,7 @@ SPDX-License-Identifier: MIT
                         </div>
                     </template>
                 </Select>
-                <Button class="blue" @click="hostBattle">Host Battle</Button>
+                <Button class="blue" @click="hostBattle">{{ t("lobby.components.battle.hostBattle.hostButton") }}</Button>
             </template>
         </div>
     </Modal>
@@ -35,12 +42,15 @@ SPDX-License-Identifier: MIT
 
 <script lang="ts" setup>
 import { computed, Ref, ref } from "vue";
+import { useTypedI18n } from "@renderer/i18n";
 
 import Loader from "@renderer/components/common/Loader.vue";
 import Modal from "@renderer/components/common/Modal.vue";
 import Button from "@renderer/components/controls/Button.vue";
 import Select from "@renderer/components/controls/Select.vue";
 import Flag from "@renderer/components/misc/Flag.vue";
+
+const { t } = useTypedI18n();
 
 const regions = ref([
     { name: "Europe", code: "EU" },
