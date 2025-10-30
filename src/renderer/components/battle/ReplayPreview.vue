ac619044efa60458678af6907648e9bdfee910a4
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2025-10-30 19:06:15 -0400

    Updated i18n for ReplayPreview

--- src/renderer/components/battle/ReplayPreview.vue
+++ src/renderer/components/battle/ReplayPreview.vue
@@ -118,11 +118,11 @@ const teams = computed<Map<number, (DemoModel.Info.Player | DemoModel.Info.AI)[]
 const extraDetails = computed(() => {
     return [
         {
-            title: "Engine Version",
+            title: t('lobby.components.battle.replayPreview.game-version'),
             data: props.replay.engineVersion,
         },
         {
-            title: "Game Version",
+            title: t('lobby.components.battle.replayPreview.game-version'),
             data: props.replay.gameVersion,
         },
     ];
