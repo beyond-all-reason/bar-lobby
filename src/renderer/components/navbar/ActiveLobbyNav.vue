452c5ac9efa12c0e0a5b23f9b170b10d73dec9c7
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2025-11-14 20:04:20 -0500

    Update ActiveLobbyNav per UI request
    Changes to the new component to better suit UI team feedback

--- src/renderer/components/navbar/ActiveLobbyNav.vue
+++ src/renderer/components/navbar/ActiveLobbyNav.vue
@@ -7,15 +7,26 @@ SPDX-License-Identifier: MIT
 <template>
     <div>
         <div class="flex-row padding-left-sm padding-right-xs">
-            <MapTinyPreview :map-name="lobbyStore.activeLobby?.mapName" :alignment="'center'" />
-            <div class="flex-col inner-size padding-left-sm">
+            <div><MapTinyPreview :map-name="lobbyStore.activeLobby?.mapName" :alignment="'center'" /></div>
+            <div class="flex-col inner-size padding-left-sm padding-bottom-sm flex-space-around">
                 <p class="lobby-title inner-size txt-md padding-left-xs padding-top-sm padding-bottom-sm">
                     {{ lobbyStore.activeLobby.name }}
                 </p>
-                <div class="flex-row flex-center-items txt-sm" style="gap: 2px">
-                    <Icon :icon="account" height="17" />{{ lobbyStore.activeLobby.playerCount }} /
-                    {{ lobbyStore.activeLobby.maxPlayerCount }}
+                <div class="flex-row flex-align-start txt-sm" style="gap: 2px">
+                    <div class="flex-row flex-center-items padding-right-md">
+                        <Icon :icon="account" height="17" class="padding-right-sm" /> {{ lobbyStore.activeLobby.playerCount }} /
+                        {{ lobbyStore.activeLobby.maxPlayerCount }}
+                    </div>
+                    <div class="flex-row flex-center-items padding-right-md">
+                        <Icon :icon="eye" height="17" class="padding-right-sm" /> {{ lobbyStore.activeLobby.spectatorCount }}
+                    </div>
+                    <div class="flex-row flex-center-items"><i>In-Queue</i></div>
                 </div>
+            </div>
+            <div class="padding-md">
+                <Panel :no-padding="true" class="fullheight margin-md flex-center-items">
+                    <p>Return To Lobby</p>
+                </Panel>
             </div>
         </div>
     </div>
@@ -26,6 +37,9 @@ import MapTinyPreview from "@renderer/components/maps/MapTinyPreview.vue";
 import { lobbyStore } from "@renderer/store/lobby.store";
 import { Icon } from "@iconify/vue";
 import account from "@iconify-icons/mdi/account";
+//import swordcross from "@iconify-icons/mdi/sword-cross";
+import eye from "@iconify-icons/mdi/eye";
+import Panel from "@renderer/components/common/Panel.vue";
 </script>
 
 <style>
@@ -36,9 +50,14 @@ import account from "@iconify-icons/mdi/account";
 }
 
 .outer-size {
-    max-width: 128px;
+    max-width: 300px;
 }
 .inner-size {
-    max-width: 96px;
+    width: 300px;
+}
+.icon-overlay {
+}
+.center-content {
+    align-content: center;
 }
 </style>
