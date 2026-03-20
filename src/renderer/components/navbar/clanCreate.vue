7a50725325d51c4c597d94868d1635ea546556b4
Rimek86 <Rimek86@t-online.de>
2026-03-20 23:46:30 +0100

    Style moved to css

--- src/renderer/components/navbar/clanCreate.vue
+++ src/renderer/components/navbar/clanCreate.vue
@@ -3,35 +3,39 @@ SPDX-FileCopyrightText: 2025 The BAR Lobby Authors
 
 SPDX-License-Identifier: MIT
 -->
-
 <template>
     <Modal :title="t('lobby.navbar.clan.create.title')" @open="openModal">
         <div class="gridform">
             <div>Name:</div>
-            <Textbox v-model="newClanData.name" placeholder="3 up to 30 characters..." maxlength="30" style="width: 100%" />
+            <Textbox
+                v-model="newClanData.name"
+                :placeholder="t('lobby.navbar.clan.create.formPlaceholderClanName')"
+                maxlength="30"
+                class="full-width"
+            />
             <div></div>
             <div
-                :style="{
-                    color: newClanData.name.length >= minClanName && newClanData.name.length <= maxClanName ? 'green' : 'red',
-                    fontSize: '0.9em',
-                    marginTop: '4px',
-                }"
+                :class="[
+                    newClanData.name.length >= minClanName && newClanData.name.length <= maxClanName ? 'valid' : 'invalid',
+                    'text-style',
+                ]"
             >
                 {{ (newClanData.name ?? "").length }}/{{ maxClanName }}
             </div>
             <div>Tag:</div>
-            <Textbox v-model="newClanData.tag" placeholder="3 up to 10 characters..." maxlength="10" style="width: 100%" />
+            <Textbox
+                v-model="newClanData.tag"
+                :placeholder="t('lobby.navbar.clan.create.formPlaceholderClanTag')"
+                maxlength="10"
+                class="full-width"
+            />
             <div></div>
             <div
-                :style="{
-                    color: newClanData.tag.length >= minClanTag && newClanData.tag.length <= maxClanTag ? 'green' : 'red',
-                    fontSize: '0.9em',
-                    marginTop: '4px',
-                }"
+                :class="[newClanData.tag.length >= minClanTag && newClanData.tag.length <= maxClanTag ? 'valid' : 'invalid', 'text-style']"
             >
                 {{ (newClanData.tag ?? "").length }}/{{ maxClanTag }}
             </div>
-            <div>Language: <Flag :countryCode="newClanData.language" style="width: 20px" /></div>
+            <div>Language: <Flag :countryCode="newClanData.language" class="flag-icon" /></div>
             <Select
                 :modelValue="newClanData.language"
                 :options="selections"
@@ -40,23 +44,32 @@ SPDX-License-Identifier: MIT
                 :placeholder="newClanData.language"
                 :filter="true"
                 @update:model-value="onUpdateSelection"
-                style="width: 100%"
+                class="full-width"
             />
             <div>Descrition:</div>
             <Textarea
                 v-model="newClanData.description"
-                placeholder="Write here with up to 500 characters the most importent infos of your clan..."
+                :placeholder="t('lobby.navbar.clan.create.formPlaceholderClanDescription')"
                 maxlength="500"
-                style="width: 100%; min-height: 100%; height: 250px"
+                class="textarea-style"
+                style="min-height: 100%"
             />
             <div></div>
-            <div style="font-size: 0.9em; color: #999; margin-top: 4px">
+            <div
+                :class="[
+                    (newClanData.description ?? '').length >= minClanDescription &&
+                    (newClanData.description ?? '').length <= maxClanDescription
+                        ? 'valid'
+                        : 'invalid',
+                    'text-style',
+                ]"
+            >
                 {{ (newClanData.description ?? "").length }}/{{ maxClanDescription }}
             </div>
             <div></div>
             <Button v-on:click="clickCreate">Create</Button>
             <div></div>
-            <div :style="{ color: clanCreateSuccess ? 'green' : 'red', fontSize: '0.9em', marginTop: '4px' }" v-html="message"></div>
+            <div :class="clanCreateSuccess ? 'success-message' : 'error-message'" v-html="message"></div>
         </div>
     </Modal>
 </template>
@@ -82,47 +95,86 @@ const newClanData = ref<ClanCreateRequestData>({
     description: "",
 });
 
+// Available languages for clans
 const selections = ref([
     { name: "International", value: "??" },
     { name: "English", value: "gb" },
     { name: "German", value: "de" },
     { name: "French", value: "fr" },
 ]);
+
+// Update event if user select a new language
 function onUpdateSelection(newSelection: string) {
     newClanData.value.language = newSelection;
 }
 
+// Min/Max values to prevent input errors
 const minClanName = 3;
 const maxClanName = 30;
 const minClanTag = 3;
 const maxClanTag = 10;
+const minClanDescription = 0;
 const maxClanDescription = 500;
 
+// Handle clan create click event with error handling
 const clanCreateSuccess = ref(false);
-
 async function clickCreate() {
     if (newClanData.value.name.length < minClanName) {
-        message.value = `Clan name must be at least ${minClanName} characters long.`;
+        message.value = t("lobby.navbar.clan.create.clanNameLengthError", { min: minClanName });
         clanCreateSuccess.value = false;
         return;
     }
     if (newClanData.value.tag.length < minClanTag) {
-        message.value = `Clan tag must be at least ${minClanTag} characters long.`;
+        message.value = t("lobby.navbar.clan.create.clanTagLengthError", { min: minClanTag });
         clanCreateSuccess.value = false;
         return;
     }
     clanCreateSuccess.value = await clanfuncs.createClan(newClanData.value);
     if (clanCreateSuccess.value) {
         newClanData.value = { name: "", tag: "", language: "??", description: "" };
-        message.value = "Clan successfully created.";
+        message.value = t("lobby.navbar.clan.create.clanCreateSuccessMessage");
     } else {
-        message.value = "Error creating the clan.<br/>(Name or tag has already been taken!)";
+        message.value = t("lobby.navbar.clan.create.clanCreateErrorMessage");
     }
 }
 
+// Reset form if modal is opened
 function openModal() {
     newClanData.value = { name: "", tag: "", language: "??", description: "" };
     message.value = "";
-    clanCreateSuccess.value = false; // Reset success state
+    clanCreateSuccess.value = false;
 }
 </script>
+
+<style scoped>
+.full-width {
+    width: 100%;
+}
+.valid {
+    color: green;
+}
+.invalid {
+    color: red;
+}
+.text-style {
+    font-size: 0.9em;
+    margin-top: 4px;
+}
+.flag-icon {
+    width: 20px;
+}
+.textarea-style {
+    width: 100%;
+    height: 250px;
+}
+.success-message {
+    color: green;
+    font-size: 0.9em;
+    margin-top: 4px;
+}
+.error-message {
+    color: red;
+    font-size: 0.9em;
+    margin-top: 4px;
+}
+</style>
