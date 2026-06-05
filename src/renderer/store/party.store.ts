62da0983a2534176036178350805ca6c7421735f
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2026-06-05 17:48:52 -0400

    Temporary hardcode
    Tachyon protocol has pending PR to correct schema, but until then we have temporarily hardcoded some default values for `party/create` responses.

--- src/renderer/store/party.store.ts
+++ src/renderer/store/party.store.ts
@@ -77,8 +77,15 @@ async function requestCreate() {
     try {
         const response = await window.tachyon.request("party/create");
         console.log("Tachyon: party/create:", response);
-        // This requires changes to Tachyon in PR 142
-        partyStore.parties.set(response.data.partyId, { ...response.data, seen: false });
+        //partyStore.parties.set(response.data.partyId, { ...response.data, seen: false });
+        // The line above is the right way, but requires changes to Tachyon in PR 142 to switch this over.
+        partyStore.parties.set(response.data.partyId, {
+            id: response.data.partyId,
+            members: [{ userId: me.userId, joinedAt: 0 }],
+            maxMembers: 3,
+            invited: [],
+            seen: false,
+        });
         parsePartyData();
     } catch (error) {
         console.error("Tachyon error: party/create:", error);
