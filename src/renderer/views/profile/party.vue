9020e2f8ad1ff19010f69cc7b16a6ecc61b6ab0e
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2026-06-21 21:51:42 -0400

    Added party members/invitees
    Party invitations now displayed with Accept/Decline buttons and current members.
    Party member component has a kick button.
    Party invitee component has a cancel invite button.

--- src/renderer/views/profile/party.vue
+++ src/renderer/views/profile/party.vue
@@ -17,22 +17,36 @@ SPDX-License-Identifier: MIT
                 <div v-if="showParty">
                     <div class="flex-row gap-md">
                         <div>
-                            <h2>Party Information</h2>
-                            <p>
-                                Current members: {{ partyStore.activeParty?.members.length || 0 }} /
-                                {{ partyStore.activeParty?.maxMembers || 0 }}
-                            </p>
-                            <p>Active invitations: {{ partyStore.activeParty?.invited.length || 0 }}</p>
-                            <Button @click="leaveParty">Leave Party</Button>
+                            <Button @click="leaveParty" class="red">Leave Party</Button>
+                            <h3>Party Members</h3>
+                            <div v-for="member in partyStore.activeParty?.members" :key="member.userId">
+                                <PartyMember :userId="member.userId" />
+                            </div>
+                            <h3>Party Invitations</h3>
+                            <div v-for="invite in partyStore.activeParty?.invited" :key="invite.userId">
+                                <PartyInvitee :userId="invite.userId" />
+                            </div>
                         </div>
                         <PartyChat />
                     </div>
                 </div>
                 <div v-else-if="showInvites">
-                    <div v-for="[partyId, party] of partyStore.parties" :key="partyId">
-                        <span>{{ party.id }} with {{ party.members.length }} members</span>
-                        <Button @click="acceptInvite(partyId)">Accept</Button>
-                        <Button @click="declineInvite(partyId)">Decline</Button>
+                    <div v-for="[partyId] of partyStore.parties" :key="partyId">
+                        <div class="flex-col">
+                            <div>
+                                Party invite received from:
+                                <span v-for="name in getPartyMemberNames(partyId)" :key="name" class="margin-right-sm">{{ name }}</span>
+                            </div>
+                            <div class="flex-row padding-md">
+                                <Button @click="acceptInvite(partyId)" class="margin-right-md green">Accept</Button>
+                                <Button @click="declineInvite(partyId)" class="margin-left-md red">Decline</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div v-else>
+                    <div class="flex-col">
+                        <div>No active parties or invites.</div>
                     </div>
                 </div>
             </Panel>
@@ -50,6 +64,12 @@ import { PartyId } from "tachyon-protocol/types";
 import Button from "@renderer/components/controls/Button.vue";
 import { useRouter } from "vue-router";
 const router = useRouter();
+import PartyMember from "@renderer/components/party/PartyMember.vue";
+import PartyInvitee from "@renderer/components/party/PartyInvitee.vue";
+import { db } from "@renderer/store/db";
+import { useDexieLiveQueryWithDeps } from "@renderer/composables/useDexieLiveQuery";
+import { UserId } from "tachyon-protocol/types";
+import { User } from "@main/model/user";
 // const { t } = useTypedI18n();
 
 const showInvites = computed(() => {
@@ -74,6 +94,32 @@ async function leaveParty() {
     if (partyStore.state === PlayersPartyState.None) {
         router.push("/play/menu");
     }
+}
+function getPartyMemberNames(partyId: PartyId) {
+    const memberIds = partyStore.parties.get(partyId)?.members.map((member) => member.userId);
+    const arr: string[] = [];
+    if (!displayNames.value) return [];
+    for (const userId of memberIds ?? []) {
+        arr.push(displayNames.value.get(userId) ?? userId);
+    }
+    return arr;
+}
+const displayNames = useDexieLiveQueryWithDeps(partyStore.parties, async () => {
+    const map = new Map<UserId, string>();
+    await db.users
+        .filter((user: User) => displayUsersFilter(user))
+        .each(function (user) {
+            map.set(user.userId, user.username);
+        });
+    return map;
+});
+
+function displayUsersFilter(user: User) {
+    if (!user) return false;
+    for (const party of partyStore.parties.values()) {
+        if (party.members.some((member) => member.userId === user.userId)) return true;
+    }
+    return false;
 }
 </script>
 
