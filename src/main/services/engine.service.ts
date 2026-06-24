903ad2c5ef499297c3c28f403232bf3435e457b4
Yeo-Jun Yoon <yeojuny@users.noreply.github.com>
2026-06-25 08:03:01 +0900

    Format engine service handler

--- src/main/services/engine.service.ts
+++ src/main/services/engine.service.ts
@@ -15,9 +15,7 @@ async function reinit() {
 }
 
 function registerIpcHandlers() {
-    ipcMain.handle("engine:listAvailableVersions", () =>
-        engineContentAPI.getLobbySelectableVersions().sort((a, b) => a.id.localeCompare(b.id))
-    );
+    ipcMain.handle("engine:listAvailableVersions", () => engineContentAPI.getLobbySelectableVersions().sort((a, b) => a.id.localeCompare(b.id)));
     ipcMain.handle("engine:downloadEngine", (_, version?: string) => engineContentAPI.downloadEngine(version));
     ipcMain.handle("engine:isVersionInstalled", (_, id: string) => engineContentAPI.isVersionInstalled(id));
     ipcMain.handle("engine:uninstallVersion", (_, version: EngineVersion) => engineContentAPI.uninstallVersion(version));
