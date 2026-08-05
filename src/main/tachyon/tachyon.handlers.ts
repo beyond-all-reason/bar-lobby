1e50c22248776e6fe2b4bf0d29da20a8e4e2d101
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2026-08-05 15:19:30 -0400

    Update tachyon.handlers.ts

--- src/main/tachyon/tachyon.handlers.ts
+++ src/main/tachyon/tachyon.handlers.ts
@@ -5,7 +5,10 @@
 import { BarIpcWebContents } from "@main/typed-ipc";
 import { logger } from "@main/utils/logger";
 import { TachyonClientRequestHandlers } from "./tachyon-client";
-import { BattleStartRequestData, MatchmakingCheckAssetsRequestData } from "tachyon-protocol/types";
+import { BattleStartRequestData, MatchmakingCheckAssetsOkResponseData, MatchmakingCheckAssetsRequestData } from "tachyon-protocol/types";
+import { gameContentAPI } from "@main/content/game/game-content";
+import { mapContentAPI } from "@main/content/maps/map-content";
+import { engineContentAPI } from "@main/content/engine/engine-content";
 
 const log = logger("tachyon-handlers");
 
@@ -34,10 +37,30 @@ function createMatchmakingHandlers(): Pick<TachyonClientRequestHandlers, "matchm
     return {
         "matchmaking/checkAssets": async (data: MatchmakingCheckAssetsRequestData) => {
             log.info(`Received matchmaking check assets request: ${JSON.stringify(data)}`);
+            let itemsRequired: boolean = false;
+            if (!gameContentAPI.isVersionInstalled(data.game)) {
+                itemsRequired = true;
+            }
+            for (const map of data.maps) {
+                if (!mapContentAPI.isVersionInstalled(map)) {
+                    itemsRequired = true;
+                    break;
+                }
+            }
+            for (const engine of data.engines) {
+                if (!engineContentAPI.isVersionInstalled(engine)) {
+                    itemsRequired = true;
+                    break;
+                }
+            }
+            // Technically we can return "downloading" if any assets are missing, but for now we just return "missing" or "complete
+            const result: MatchmakingCheckAssetsOkResponseData = {
+                assetStatus: itemsRequired ? "missing" : "complete",
+            };
             return {
-                status: "failed",
-                reason: "command_unimplemented",
-                details: "This client does not yet check assets, and instead auto-fails.",
+                status: "success",
+                result: result,
+                invalid: true,
             };
         },
     };
