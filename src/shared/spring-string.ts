9316e82dcdec36d0513e23c7794104214b15e9a8
Nathaniel Mitchell <nathaniel.mitchell@gmail.com>
2026-09-02 12:19:03 -0400

    Temporary backwards compat
    Server needs updated

--- src/shared/spring-string.ts
+++ src/shared/spring-string.ts
@@ -10,7 +10,17 @@ export interface SpringConnectionDetails {
     battleId: string;
 }
 
-export function createSpringString({ ips, port, username, password }: SpringConnectionDetails): string {
-    // Note, data.ips[string] values are either ipv4 or ipv6, but we only use the first (only) for now.
+// TODO: Remove the backward compatibility for the `ip` field once the server is updated to Tachyon 1.24.0
+// @ts-expect-error ip is not part of SpringConnectionDetails but is used for backward server compatibility
+export function createSpringString({ ip, ips, port, username, password }: SpringConnectionDetails): string {
+    if (ips.length === 0) {
+        if (ip) {
+            //Temporary guard because server needs to update to Tachyon 1.24.0
+            ips.push(ip);
+        } else {
+            throw new Error("No IPs provided for SpringString connection");
+        }
+    }
+    // We only use the first provided IP for now, it will typically be the only one provided.
     return `spring://${username}:${password}@${ips[0]}:${port}`;
 }
